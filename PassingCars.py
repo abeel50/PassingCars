73fccd1a9cddd9f1b5ddc112108eab82966202b0
abeel50 <abeelmuhammad50@gmail.com>
2022-06-15 22:36:19 +0500

    50%

--- PassingCars.py
+++ PassingCars.py
@@ -1,11 +1,19 @@
-from itertools import combinations
+def countPairs(A, start):
+    print(start)
+    if start >= len(A):
+        return 0
+    if A[start] == 0:
+        p = A[start + 1:].count(1)
+        # print (p)
+        return p + countPairs(A, start + 1)
+    return 0 + countPairs(A, start+1)
 
 def solution(A):
     # write your code in Python 3.6
-    comb = combinations(range(0,len(A)),2)
-    pairs = 0
-    for i,j in comb:
-        if A[i] == 0 and A[j] == 1:
-            # print(i,j, A[i]+A[j])
-            pairs += 1
-    return pairs
+    # comb = combinations(range(0,len(A)),2)
+    # pairs = 0
+    # for i,j in comb:
+    #     if A[i] == 0 and A[j] == 1:
+    #         # print(i,j, A[i]+A[j])
+    #         pairs += 1
+    return countPairs(A,0)
